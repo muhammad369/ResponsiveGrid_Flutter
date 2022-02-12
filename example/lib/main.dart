e9df9e6a2cfe41900b7f846799f583a1c5854a63
mohamed ali <mohamed@ali.com>
2022-02-12 20:37:13 +0200

    upgraded project dependencies - recreated the example app - tested for web

---
+++ example/lib/main.dart
@@ -0,0 +1,169 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:responsive_grid/responsive_grid.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+  final Widget? homeWidget;
+
+  MyApp({this.homeWidget});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: homeWidget ?? MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: _buildGridList());
+  }
+
+  Widget _buildGridList() {
+    return ResponsiveGridList(
+        rowMainAxisAlignment: MainAxisAlignment.center,
+        desiredItemWidth: 100,
+        minSpacing: 10,
+        children: [
+          1,
+          2,
+          3,
+          4,
+          5,
+          6,
+          7,
+          8,
+          9,
+          10,
+          11,
+          12,
+          13,
+          14,
+          15,
+          16,
+          17,
+          18,
+          19,
+          20
+        ].map((i) {
+          return Container(
+            height: ((i%5) +1) * 100.0,
+            alignment: Alignment(0, 0),
+            color: Colors.cyan,
+            child: Text(i.toString()),
+          );
+        }).toList());
+  }
+
+  Widget _buildGridLayout() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: <Widget>[
+        ResponsiveGridRow(
+          children: [
+            ResponsiveGridCol(
+              lg: 12,
+              child: Container(
+                height: 100,
+                alignment: Alignment(0, 0),
+                color: Colors.purple,
+                child: Text("lg : 12"),
+              ),
+            ),
+            ResponsiveGridCol(
+              xs: 6,
+              md: 3,
+              child: Container(
+                height: 100,
+                alignment: Alignment(0, 0),
+                color: Colors.green,
+                child: Text("xs : 6 \r\nmd : 3"),
+              ),
+            ),
+            ResponsiveGridCol(
+              xs: 6,
+              md: 3,
+              child: Container(
+                height: 100,
+                alignment: Alignment(0, 0),
+                color: Colors.orange,
+                child: Text("xs : 6 \r\nmd : 3"),
+              ),
+            ),
+            ResponsiveGridCol(
+              xs: 6,
+              md: 3,
+              child: Container(
+                height: 100,
+                alignment: Alignment(0, 0),
+                color: Colors.red,
+                child: Text("xs : 6 \r\nmd : 3"),
+              ),
+            ),
+            ResponsiveGridCol(
+              xs: 6,
+              md: 3,
+              child: Container(
+                height: 100,
+                alignment: Alignment(0, 0),
+                color: Colors.blue,
+                child: Text("xs : 6 \r\nmd : 3"),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget _buildGridLayout_testCrossAlign() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: <Widget>[
+        ResponsiveGridRow(
+          children: [
+            ResponsiveGridCol(
+
+              xs: 6,
+              child: Column(
+                children: [
+                  Container(height: 100, color: Colors.blue, margin: EdgeInsets.all(10),),// height 100px
+                  Container(height: 100, color: Colors.blueGrey, margin: EdgeInsets.all(10),) // height 100px
+                ],
+              ),
+            ),
+            ResponsiveGridCol(
+
+              xs: 6,
+              child: Container(height: 400, color: Colors.black45, margin: EdgeInsets.all(10),), // height 500px
+            )
+          ],
+        ),
+
+      ],
+    );
+  }
+
+}
